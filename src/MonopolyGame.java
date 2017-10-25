c8315c57178732e74418cbc6d6d885d9ea7b2a1d
Erman Havuç <ermanhavuc@gmail.com>
2017-10-26 01:18:27 +0300

    ArrayList to Array for players

--- src/MonopolyGame.java
+++ src/MonopolyGame.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -10,28 +9,35 @@ public class MonopolyGame {
         // oyun alanı kurulumu
 
         private Board board = new Board();                                  // board oluştu
-        private Die[] dice = new Die[DiceNumber];                           // zar objeleri yaratıldı
+        private Die[] dice = new Die[DiceNumber];   // zar objeleri yaratıldı
 
 
     public MonopolyGame(int numOfPlayer) {
 
-        ArrayList<Player> players = new ArrayList<Player>(numOfPlayer); // playerları oluşturduk
+        Player[] players = new Player[numOfPlayer];
+        Piece[] pieces = new Piece[numOfPlayer];
+
             for (int i = 0; i < DiceNumber; i++) {
                 dice[i] = new Die();
             }
+
              System.out.println("Enter names of Players: ");
+
             for (int i = 0; i < numOfPlayer; i++) {
                 Scanner names = new Scanner(System.in);
                 String nameOfPlayer = names.nextLine();
-                Player player = new Player(nameOfPlayer, board, dice, new Piece(board.getStartSquare(),i));
-                players.add(player);
+                pieces[i] = new Piece(board.getStartSquare(),i);
+                players[i] = new Player(nameOfPlayer, board, dice, pieces[i]);
+                System.out.println(players[i].getPieceName());
             }
             playGame(players);
         }
 
-    public void playGame(ArrayList<Player> players) {
+    public void playGame(Player[] players) {
+
         for (int i = 0; i < RoundsNumber; i++) {
             System.out.println("---Round "+i+"---");
+
             for (Player player : players) {
                 player.takeTurn();
             }
